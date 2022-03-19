453affe8b6250ba6c315265c05899b2e1f0d96dc
Andy Huang <andy2244759@gmail.com>
2022-03-19 14:08:25 +0800

    for loop version

--- odd.py
+++ odd.py
@@ -1,11 +1,10 @@
-def add_odd_2():
+def add_odd_3():
     number1 = int(input("please enter a number: "))
     sum2 = 0
     i = 1
-    while i <= number1:
+    for i in range(number1+1):
         if (i % 2) != 0:
             sum2 += i
-        i += 1
     print(f"Summation from 1 to {number1} is {sum2}")
 
-add_odd_2()
+add_odd_3()
